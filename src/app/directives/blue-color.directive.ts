ed1833d2e177edc615ff8c0753f3b7d7d7608925
dev belal_Naeim <AmarRizk.820@azhar.edu.eg>
2022-07-07 14:27:17 +0200

    Initial commit

---
+++ src/app/directives/blue-color.directive.ts
@@ -0,0 +1,32 @@
+import { Directive, ElementRef, HostListener, Renderer2 } from "@angular/core";
+//renderer 2 examples https://www.concretepage.com/angular-2/angular-4-renderer2-example
+@Directive({
+  selector: "[appBlueColor]"
+})
+export class BlueColorDirective {
+  constructor(private element: ElementRef, private render: Renderer2) {
+    console.log(element);
+    element.nativeElement.style.color = "blue";
+  }
+  newDiv = this.render.createElement("div");
+  textContent = this.render.createText("Hello sample div");
+
+  @HostListener("mouseenter")
+  onMouseEnter() {
+    this.highlight("yellow");
+    this.render.setStyle(this.element.nativeElement, "color", "red");
+
+    this.render.appendChild(this.newDiv, this.textContent);
+    this.render.appendChild(this.element.nativeElement, this.newDiv);
+  }
+
+  @HostListener("mouseleave")
+  onMouseLeave() {
+    this.highlight(null);
+    this.render.removeChild(this.element.nativeElement, this.newDiv);
+  }
+
+  private highlight(color: string) {
+    this.element.nativeElement.style.backgroundColor = color;
+  }
+}
